e2420333cd7ca8f6fce0ab48ec1f3a2c390ddc12
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-01 14:32:00 +0300

    Enhance HomeLoaded state to include selectedTheme property with default value and implement copyWith method for state management

--- lib/features/home/presentation/cubit/home_state.dart
+++ lib/features/home/presentation/cubit/home_state.dart
@@ -9,7 +9,23 @@ abstract class HomeState extends Equatable {
 
 class HomeInitial extends HomeState {}
 class HomeLoading extends HomeState {}
-class HomeLoaded extends HomeState {}
+
+class HomeLoaded extends HomeState {
+  final String selectedTheme;
+  const HomeLoaded({this.selectedTheme = 'Food'});
+
+  HomeLoaded copyWith({
+    String? selectedTheme,
+  }) {
+    return HomeLoaded(
+      selectedTheme: selectedTheme ?? this.selectedTheme,
+    );
+  }
+
+  @override
+  List<Object> get props => [selectedTheme];
+}
+
 class HomeError extends HomeState {
   final String message;
   const HomeError(this.message);
