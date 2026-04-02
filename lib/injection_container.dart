58249d68917c1a944a196ac60f954e05ea7bc7b2
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-02 20:53:25 +0300

    Enhance injection container by adding MultiplayerCubit and updating GameCubit registration to lazy singleton, improving dependency management for multiplayer functionality.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -4,17 +4,19 @@ import 'package:hidden_word/features/home/presentation/cubit/home_cubit.dart';
 import 'package:hidden_word/features/join_room/presentation/cubit/join_room_cubit.dart';
 import 'package:hidden_word/features/room_lobby/presentation/cubit/room_lobby_cubit.dart';
 import 'package:hidden_word/features/game/presentation/cubit/game_cubit.dart';
+import 'package:hidden_word/features/multiplayer/presentation/cubit/multiplayer_cubit.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
   //! Features
-  // Cubit
+  // Cubits
   sl.registerFactory(() => HomeCubit());
   sl.registerFactory(() => GameLobbyCubit());
   sl.registerFactory(() => RoomLobbyCubit());
   sl.registerFactory(() => JoinRoomCubit());
-  sl.registerFactory(() => GameCubit());
+  sl.registerLazySingleton(() => GameCubit());
+  sl.registerFactory(() => MultiplayerCubit(gameCubit: sl()));
 
   // Use cases
 
