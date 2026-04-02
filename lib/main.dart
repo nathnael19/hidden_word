82563855fc88b65350737c6493e616995f1674ba
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-02 19:59:45 +0300

    Add GameCubit to main.dart for improved state management in game feature

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:hidden_word/features/game_lobby/presentation/cubit/game_lobby_cu
 import 'package:hidden_word/features/home/presentation/cubit/home_cubit.dart';
 import 'package:hidden_word/features/join_room/presentation/cubit/join_room_cubit.dart';
 import 'package:hidden_word/features/room_lobby/presentation/cubit/room_lobby_cubit.dart';
+import 'package:hidden_word/features/game/presentation/cubit/game_cubit.dart';
 import 'package:hidden_word/features/splash/presentation/pages/splash_page.dart';
 import 'injection_container.dart' as di;
 
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => di.sl<GameLobbyCubit>()),
         BlocProvider(create: (_) => di.sl<RoomLobbyCubit>()),
         BlocProvider(create: (_) => di.sl<JoinRoomCubit>()),
+        BlocProvider(create: (_) => di.sl<GameCubit>()),
       ],
       child: MaterialApp(
         title: 'Hidden Word',
