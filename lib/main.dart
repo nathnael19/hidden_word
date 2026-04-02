96ec0458e7aa576fdc3f674acb6e14bb9c8ec350
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-02 20:53:41 +0300

    Add MultiplayerCubit to main.dart for multiplayer functionality integration, enhancing the app's capability to support multiplayer game sessions.

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:hidden_word/features/home/presentation/cubit/home_cubit.dart';
 import 'package:hidden_word/features/join_room/presentation/cubit/join_room_cubit.dart';
 import 'package:hidden_word/features/room_lobby/presentation/cubit/room_lobby_cubit.dart';
 import 'package:hidden_word/features/game/presentation/cubit/game_cubit.dart';
+import 'package:hidden_word/features/multiplayer/presentation/cubit/multiplayer_cubit.dart';
 import 'package:hidden_word/features/splash/presentation/pages/splash_page.dart';
 import 'injection_container.dart' as di;
 
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => di.sl<RoomLobbyCubit>()),
         BlocProvider(create: (_) => di.sl<JoinRoomCubit>()),
         BlocProvider(create: (_) => di.sl<GameCubit>()),
+        BlocProvider(create: (_) => di.sl<MultiplayerCubit>()),
       ],
       child: MaterialApp(
         title: 'Hidden Word',
