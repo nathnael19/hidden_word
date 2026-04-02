ebc33cc2228bc3e5c2650f36ca1155796195b29a
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-02 16:48:50 +0300

    Add JoinRoomCubit registration to the injection container for improved dependency management across features.

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:hidden_word/features/game_lobby/presentation/cubit/game_lobby_cubit.dart';
 import 'package:hidden_word/features/home/presentation/cubit/home_cubit.dart';
+import 'package:hidden_word/features/join_room/presentation/cubit/join_room_cubit.dart';
 import 'package:hidden_word/features/room_lobby/presentation/cubit/room_lobby_cubit.dart';
 
 final sl = GetIt.instance;
@@ -11,6 +12,7 @@ Future<void> init() async {
   sl.registerFactory(() => HomeCubit());
   sl.registerFactory(() => GameLobbyCubit());
   sl.registerFactory(() => RoomLobbyCubit());
+  sl.registerFactory(() => JoinRoomCubit());
 
   // Use cases
 
