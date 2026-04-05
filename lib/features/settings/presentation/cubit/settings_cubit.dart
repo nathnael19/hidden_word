ef0d1844bd5f86bf9d11f6b3d1eb3e6af7639baf
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-05 20:37:51 +0300

    Refactor SettingsCubit to utilize SharedPreferences for persistent settings. Added functionality to toggle sound, haptic feedback, and timer visibility, as well as set language preferences, enhancing user experience and settings management.

--- lib/features/settings/presentation/cubit/settings_cubit.dart
+++ lib/features/settings/presentation/cubit/settings_cubit.dart
@@ -1,22 +1,43 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'settings_state.dart';
 
 class SettingsCubit extends Cubit<SettingsState> {
-  SettingsCubit() : super(const SettingsState());
+  final SharedPreferences sharedPreferences;
+
+  static const _keySound = 'settings_sound';
+  static const _keyHaptic = 'settings_haptic';
+  static const _keyTimer = 'settings_timer';
+  static const _keyLanguage = 'settings_language';
+
+  SettingsCubit({required this.sharedPreferences})
+      : super(SettingsState(
+          isSoundEnabled: sharedPreferences.getBool(_keySound) ?? true,
+          isHapticEnabled: sharedPreferences.getBool(_keyHaptic) ?? true,
+          isTimerVisible: sharedPreferences.getBool(_keyTimer) ?? false,
+          language: sharedPreferences.getString(_keyLanguage) ?? 'en',
+        ));
 
   void toggleSound() {
-    emit(state.copyWith(isSoundEnabled: !state.isSoundEnabled));
+    final newValue = !state.isSoundEnabled;
+    sharedPreferences.setBool(_keySound, newValue);
+    emit(state.copyWith(isSoundEnabled: newValue));
   }
 
   void toggleHaptic() {
-    emit(state.copyWith(isHapticEnabled: !state.isHapticEnabled));
+    final newValue = !state.isHapticEnabled;
+    sharedPreferences.setBool(_keyHaptic, newValue);
+    emit(state.copyWith(isHapticEnabled: newValue));
   }
 
   void toggleTimer() {
-    emit(state.copyWith(isTimerVisible: !state.isTimerVisible));
+    final newValue = !state.isTimerVisible;
+    sharedPreferences.setBool(_keyTimer, newValue);
+    emit(state.copyWith(isTimerVisible: newValue));
   }
 
   void setLanguage(String langCode) {
+    sharedPreferences.setString(_keyLanguage, langCode);
     emit(state.copyWith(language: langCode));
   }
 }
