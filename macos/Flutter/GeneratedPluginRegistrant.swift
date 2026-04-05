0b6c76ae1aceb5723bf0acc8f0ef112748451a64
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-04-05 20:33:13 +0300

    Add SharedPreferences plugin registration in GeneratedPluginRegistrant.swift. This update integrates shared preferences support, enhancing local data storage capabilities in the application.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import bonsoir_darwin
 import connectivity_plus
 import network_info_plus
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
